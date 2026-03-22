40e34503a37655bb4587da9eda5b553951f7ecc8
AlvarezRoss <alvarezoteromartin@gmail.com>
2026-03-22 14:08:22 +0100

    Added animation change logic and loading sprite frames and stats from the stat roll scene

--- Scripts/Char.cs
+++ Scripts/Char.cs
@@ -11,32 +11,55 @@ public partial class Char : CharacterBody2D
 	bool moving;
 	Area2D clickArea;
 	Stats stats;
-
-	Char(Stats stats) // Constructor
+	AnimatedSprite2D animatedSprite;
+	SpriteFrames frames;
+	Directions movementDirection;
+	Directions lastDirection;
+	public void Init(Stats stats, SpriteFrames frames)
 	{
-		this.stats = stats; 
+		this.frames = frames;
+		this.stats = stats;
 	}
 	public override void _Ready()
 	{
 		clickArea = GetNode<Area2D>("ClickArea");
-		clickArea.InputEvent += _on_click_area_input_event;
-	}
+        clickArea.InputEvent += _on_click_area_input_event;
+		animatedSprite = GetNode<AnimatedSprite2D>("CharSprite");
+		animatedSprite.SpriteFrames = frames;
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 
-
+	public enum Directions
+	{
+		Idle,
+		Up,
+		RightUp,
+		LeftUp,
+		Right,
+		Left,
+		Down,
+		LeftDown,
+		RightDown
+	}
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
 		Velocity = Position.DirectionTo(movementTarget) * speed;
-		if (Position.DistanceTo(movementTarget) > 10 && moving)
+		if (Position.DistanceTo(movementTarget) > 20 && moving)
 		{
+			lastDirection = movementDirection;
 			MoveAndSlide();
+			
 		}
 		else
 		{
+			movementDirection = Directions.Idle;
 			moving = false;
+			
 		}
+
+        Animation();
     }
 
     public override void _Input(InputEvent @event)
@@ -46,7 +69,8 @@ public partial class Char : CharacterBody2D
 		{
 			moving = true;
 			movementTarget = GetViewport().GetMousePosition();
-		}
+            movementDirection = GetDirection(); // We do it here to set the movement direction once
+        }
     }
 
 	private void _on_click_area_input_event(Node viewport, InputEvent @event, long shape_idx)
@@ -56,5 +80,109 @@ public partial class Char : CharacterBody2D
 			selected = true;
 		}
 			
+	}
+
+	public virtual void Animation()
+	{
+		if (movementDirection == Directions.Idle) IdleAnimations();
+
+		if (movementDirection == Directions.LeftUp && animatedSprite.Animation != "WalkingLeftUp")
+		{
+			animatedSprite.Play("WalkingLeftUp");
+		}
+		else if (movementDirection == Directions.LeftDown && animatedSprite.Animation != "WalkingLeftDown")
+		{
+			animatedSprite.Play("WalkingLeftDown");
+		}
+		else if (movementDirection == Directions.Left && animatedSprite.Animation != "WalkingLeft")
+		{
+			animatedSprite.Play("WalkingLeft");
+		}
+		else if (movementDirection == Directions.Right && animatedSprite.Animation != "WalkingRight")
+		{
+			animatedSprite.Play("WalkingRight");
+		}
+		else if (movementDirection == Directions.RightDown && animatedSprite.Animation != "WalkingRightDown")
+		{
+			animatedSprite.Play("WalkingRightDown");
+		}
+		else if (movementDirection == Directions.RightUp && animatedSprite.Animation != "WalkingRightUp")
+		{
+			animatedSprite.Play("WalkingRightUp");
+		}
+		else if (movementDirection == Directions.Up && animatedSprite.Animation != "WalkingUp")
+		{
+			animatedSprite.Play("WalkingUp");
+		}
+		else if (movementDirection == Directions.Down && animatedSprite.Animation != "WalkingDown")
+		{
+			animatedSprite.Play("WalkingDown");
+		}
+	}
+
+	private Directions GetDirection()
+	{
+		if (GlobalPosition.X > movementTarget.X + 40)
+		{
+			if (GlobalPosition.Y > movementTarget.Y + 40)
+			{
+				return Directions.LeftUp;
+			}
+			else if (GlobalPosition.Y < movementTarget.Y - 40)
+			{
+				return Directions.LeftDown;
+			}
+			else return Directions.Left;
+		}
+		else if (GlobalPosition.X < movementTarget.X - 40)
+		{
+			if (GlobalPosition.Y > movementTarget.Y + 40)
+			{
+				return Directions.RightUp;
+			}
+			else if (GlobalPosition.Y < movementTarget.Y - 40)
+			{
+				return Directions.RightDown;
+			}
+			else return Directions.Right;
+		}
+		else if (GlobalPosition.Y > movementTarget.Y) return Directions.Up;
+		else if (GlobalPosition.Y < movementTarget.Y) return Directions.Down;
+
+		return Directions.Idle;
+	}
+
+	private void IdleAnimations()
+	{
+		switch(lastDirection)
+		{
+			case Directions.LeftUp:
+				animatedSprite.Play("IdleLeftUp");
+				break;
+			case Directions.LeftDown:
+				animatedSprite.Play("IdleLeftDown");
+				break;
+			case Directions.Left:
+				animatedSprite.Play("IdleLeft");
+				break;
+			case Directions.RightUp:
+				animatedSprite.Play("IdleRightUp");
+				break;
+			case Directions.Right:
+				animatedSprite.Play("IdleRight");
+				break;
+			case Directions.RightDown:
+				animatedSprite.Play("IdleRightDown");
+				break;
+			case Directions.Up:
+				animatedSprite.Play("IdleUp");
+				break;
+			case Directions.Down:
+				animatedSprite.Play("IdleDown");
+				break;
+			default:
+				animatedSprite.Play("IdleDown");
+				return;
+		}
 	}
 }
